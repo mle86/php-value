00e828afcb050a2042417ba657b519124bc66fee
mle <maximilian@eul.cc>
2015-08-23 17:09:14 +0200

    added AbstractSerializableValue base subclass

---
+++ src/Value/AbstractSerializableValue.php
@@ -0,0 +1,40 @@
+<?php
+namespace mle86\Value;
+
+
+/**
+ * This extension of AbstractValue provides easy serializability
+ * for the Value objects.  It implements the PHP 5.4 JsonSerializable interface.
+ *
+ * @author Maximilian Eul
+ */
+abstract class AbstractSerializableValue
+	extends AbstractValue
+	implements \JsonSerializable
+{
+
+	/**
+	 * Returns the wrapped value -- like value(), but with an explicit
+	 * string typecast.  This allows string concatenation of Value objects.
+	 * (The typecast is necessary to prevent type mismatch errors
+	 *  for number-wrapping classes.)
+	 *
+	 * @return string
+	 */
+	public function __toString () {
+		return (string)$this->value();
+	}
+
+
+	/**
+	 * Returns the wrapped value -- like value().
+	 * This enables json_encode() to encode the Value object.
+	 *
+	 * @return mixed
+	 */
+	public function jsonSerialize () {
+		return $this->value();
+	}
+
+}
+
