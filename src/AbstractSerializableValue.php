5bcc2f59f967a1bb76484818d08bf9bd3fb3231b
mle <maximilian@eul.cc>
2019-11-28 17:37:26 +0100

    ASV: support PHP 7.4 serialization methods
    The AbstractSerializableValue base class
    uses the new __serialize and __unserialize magic methods now
    (see https://wiki.php.net/rfc/custom_object_serialization).
    
    This allows a much more compact serialization output format.
    
    This change breaks forward compatibility of earlier versions
    of this library older than v1.3/v2.2 because they won't
    understand the new serialization format.
    
    Backwards compatibility is preserved
    because this library still understands the older serialization format
    even when running in a PHP 7.4+ environment.
    
    If you have only one deployment of this library
    or if there's no persistent storage at all,
    upgrading to this release is unproblematic.
    
    If however there's a possibility that the newer serializations
    created by this release in a PHP 7.4+ environment
    will be read by a pre-v2.2 installation of this library,
    you'll have to upgrade these installation to v1.3/v2.2 or newer
    or risk incompatibility errors when unserializing.

--- src/AbstractSerializableValue.php
+++ src/AbstractSerializableValue.php
@@ -40,6 +40,64 @@ abstract class AbstractSerializableValue extends AbstractValue implements \JsonS
     }
 
     /**
+     * Custom serialization for PHP7.4+.
+     *
+     * This method outputs our new serialization format:
+     * just the stored value, nothing else.
+     * Because it's only the one property we don't have to include the prop name either.
+     * This makes the serialized form considerably shorter
+     * and human-readable.
+     *
+     * Serializations in this form are accepted by the pre-PHP7.4 {@see __wakeup} method
+     * introduced in v1.3.0/v2.2.0 of this library.
+     * Older library versions won't be able to unserialize this format!
+     *
+     * @internal
+     */
+    public function __serialize(): array
+    {
+        /*
+         * This method must have an array return type.
+         * That array may be nested, associative, whatever.
+         * But it's just one value -- so the simplest form is, of course, [0 => $value].
+         *
+         * Don't change this format!
+         * If the serialization is passed back to an older version of this library,
+         * it might not be recognizable anymore.
+         * Support for this format was added in v2.2.0 (see __wakeup).
+         */
+        return [$this->value()];
+    }
+
+    /**
+     * Unserialization helper method for PHP 7.4+.
+     *
+     * This methods reads our custom serialization format (see {@see __serialize})
+     * but for backwards compatibility it also understands the older, PHP-internal format.
+     * This is necessary to handle serializations that have been created with an older version of this library
+     * or in a pre-PHP7.4 environment.
+     *
+     * @internal
+     */
+    public function __unserialize(array $serializedInput)
+    {
+        if (array_key_exists(0, $serializedInput)) {
+            // This is the format returned by our __serialize implementation.
+            $this->__construct($serializedInput[0]);
+            return;
+        }
+
+        $legacyKey = "\0" . AbstractValue::class . "\0" . 'value';
+        if (array_key_exists($legacyKey, $serializedInput)) {
+            // This is the PHP-internal format returned by pre-7.4 serializing.
+            $this->__construct($serializedInput[$legacyKey]);
+            return;
+        }
+
+        throw new InvalidArgumentException('unknown ' . static::class . ' serialization format');
+    }
+
+    /**
      * This method ensures that unserialized instances are still valid.
      *
      * Serialization may be stored in databases for a long time;
@@ -53,6 +111,7 @@ abstract class AbstractSerializableValue extends AbstractValue implements \JsonS
      * It is only called in a PHP7.3 (or older) environment
      * as PHP 7.4+ ignores it if an __unserialize method is present.
      *
+     * @todo This method can be removed when the library has a PHP 7.4+ requirement.
      * @internal
      */
     public function __wakeup()
